29d7d25241a98cef45bfe46f0d7b3797c6829468
Thomas Kelly <tmkelly28@gmail.com>
2017-06-30 17:44:55 -0400

    fix up router

--- browser/js/Routes.js
+++ browser/js/Routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 import history from './history';
 import Root from './components/Root';
 import Home from './components/Home';
